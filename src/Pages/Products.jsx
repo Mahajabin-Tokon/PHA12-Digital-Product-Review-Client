2fb6ce4b942e309d0056de98ca92c93cb64c5d98
Mahajabin Tokon <tokonislam1122@gmail.com>
2025-01-13 12:00:00 -0700

    Products page statically built

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import { BiSolidUpvote } from "react-icons/bi";
 
 const Products = () => {
-    return (
-        <div>
-            Products
+  return (
+    <div className="max-w-6xl mx-auto my-2 px-2">
+      <form className="flex justify-center items-center gap-2">
+        <div className="py-2">
+          <div className="">
+            <input
+              name="search"
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-full"
+            />
+          </div>
         </div>
-    );
+        <div className="">
+          <input
+            type="submit"
+            value="Search"
+            className="btn w-full"
+          />
+        </div>
+      </form>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 py-5 ">
+        <div className="card bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/9T51DWT/testImg.jpg"
+              alt="Product Image"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Product Name</h2>
+            <p>Tags</p>
+            <div className="card-actions justify-end">
+              <button onClick={() => handleUpvote("123")} className="btn">
+                <BiSolidUpvote /> Count
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Products;
--- src/Sections/Trending.jsx
+++ src/Sections/Trending.jsx
@@ -74,6 +74,40 @@ const Trending = () => {
             </div>
           </div>
         </div>
+        <div className="card bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/9T51DWT/testImg.jpg"
+              alt="Product Image"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Product Name</h2>
+            <p>Tags</p>
+            <div className="card-actions justify-end">
+              <button onClick={() => handleUpvote("123")} className="btn">
+                <BiSolidUpvote /> Count
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://i.ibb.co/9T51DWT/testImg.jpg"
+              alt="Product Image"
+            />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Product Name</h2>
+            <p>Tags</p>
+            <div className="card-actions justify-end">
+              <button onClick={() => handleUpvote("123")} className="btn">
+                <BiSolidUpvote /> Count
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="w-1/2 mx-auto pb-10">
         <Link to="/products" className="btn w-full">
