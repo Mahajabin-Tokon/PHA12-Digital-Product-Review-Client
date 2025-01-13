344801525c4c4e37a67be22febb98d12e68e1d1c
Mahajabin Tokon <tokonislam1122@gmail.com>
2025-01-13 13:46:17 -0700

    My profile page complete

--- src/Layouts/Dashboard.jsx
+++ src/Layouts/Dashboard.jsx
@@ -6,6 +6,9 @@ const Dashboard = () => {
       <div className="min-h-screen">
         <ul className="menu menu-xs md:menu-md w-full bg-base-200 h-full">
           <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
             <NavLink to="/dashboard/adminHome">My Profile</NavLink>
           </li>
           <li>
--- src/Pages/Dashboard/MyProfile.jsx
+++ src/Pages/Dashboard/MyProfile.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { authContext } from "../../AuthProvider/AuthProvider";
 
 const MyProfile = () => {
-    return (
-        <div>
-            My Profile
+  const { user } = useContext(authContext);
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure>
+        <img className="md:h-80" src={user?.photoURL} alt="User Image" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{user?.displayName}</h2>
+        <p>{user?.email}</p>
+        <p>Status: Verified</p>
+        <div className="card-actions justify-end">
+          <button className="btn">$50</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MyProfile;
