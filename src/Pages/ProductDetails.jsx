bbe369317375bb5a8683234e27d5ef30d2e28d78
Mahajabin Tokon <tokonislam1122@gmail.com>
2025-01-13 19:41:15 -0700

    Product details page complete

--- src/Pages/ProductDetails.jsx
+++ src/Pages/ProductDetails.jsx
@@ -7,6 +7,7 @@ import { BiSolidUpvote } from "react-icons/bi";
 
 const ProductDetails = () => {
   const { user } = useContext(authContext);
+  const { displayName, photoURL, email } = user || {};
   const params = useParams();
   const { refetch, data: product = {} } = useQuery({
     queryKey: ["product"],
@@ -18,12 +19,55 @@ const ProductDetails = () => {
       return res.data;
     },
   });
-  console.log(product);
+  console.log(user);
+
+  const handleAddReview = async (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const reviewDescription = form.rDescription.value;
+    const reviewRating = form.rRating.value;
+
+    const review = {
+      date: new Date(),
+      displayName,
+      photoURL,
+      email,
+      productId: product?._id,
+      reviewDescription,
+      reviewRating,
+    };
+    console.log(review);
+
+    // try {
+    //   const { data } = await axios.post(
+    //     `${import.meta.env.VITE_API_URL}/addProduct`,
+    //     product
+    //   );
+
+    //   if (data.insertedId) {
+    //     Swal.fire({
+    //       title: "Success!",
+    //       text: "New Product Added Successfully!",
+    //       icon: "success",
+    //       confirmButtonText: "Cool",
+    //     });
+    //     navigate("/dashboard/myProducts");
+    //   }
+
+    //   //   console.log(data);
+    // } catch (err) {
+    //   console.log(err);
+    // }
+  };
 
   return (
-    <div className="card bg-base-100 shadow-xl">
+    <div className="card bg-base-100 shadow-xl pt-5 pb-20">
       <figure>
-        <img className="md:h-80" src={product?.productImage} alt="Product Image" />
+        <img
+          className="md:h-80"
+          src={product?.productImage}
+          alt="Product Image"
+        />
       </figure>
       <div className="card-body">
         <h2 className="card-title">{product?.productName}</h2>
@@ -35,13 +79,106 @@ const ProductDetails = () => {
         <div className="card-actions justify-end">
           <button
             onClick={() => handleUpvote("123")}
-            disabled={product?.email === user?.email}
+            disabled={product?.email === email}
             className="btn"
           >
             <BiSolidUpvote /> {product?.productUpvotes}
           </button>
         </div>
       </div>
+      {/* Review */}
+      <div className="text-center text-4xl py-10">Reviews</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 p-5">
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Review</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Review</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+        </div>
+        <div className="card bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">Reviewer Name</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+          </div>
+        </div>
+      </div>
+      {/* Add review form */}
+      <form onSubmit={handleAddReview} className="bg-base-200 py-10 rounded-xl">
+        <div className="text-center text-4xl py-10">Add a Review</div>
+        {/* Field 1 */}
+        <div className="px-10 py-2">
+          <div className="w-full">
+            <p>Reviewer Name</p>
+            <input
+              name="reviewerName"
+              type="text"
+              className="input input-bordered w-full"
+            />
+          </div>
+        </div>
+        {/* Field 2 */}
+        <div className="px-10 py-2">
+          <div className="w-full">
+            <p>Reviewer Name</p>
+            <input
+              value={displayName}
+              name="rName"
+              type="text"
+              className="input input-bordered w-full"
+            />
+          </div>
+        </div>
+        {/* Field 3 */}
+        <div className="px-10 py-2">
+          <div className="w-full">
+            <p>Reviewer Image</p>
+            <input
+              value={photoURL}
+              name="rImage"
+              type="text"
+              className="input input-bordered w-full"
+            />
+          </div>
+        </div>
+        {/* Field 4 */}
+        <div className="px-10 py-2">
+          <div className="w-full">
+            <p>Review Description</p>
+            <input
+              name="rDescription"
+              type="text"
+              placeholder="Review Description"
+              className="input input-bordered w-full"
+            />
+          </div>
+        </div>
+        {/* Field 5 */}
+        <div className="px-10 py-2">
+          <div className="w-full">
+            <p>Reviewer Rating</p>
+            <input
+              name="rRating"
+              type="number"
+              placeholder="Rating"
+              className="input input-bordered w-full"
+            />
+          </div>
+        </div>
+
+        <div className="px-10 py-4">
+          <input
+            type="submit"
+            value="Sumit"
+            className="btn w-full btn-success"
+          />
+        </div>
+      </form>
     </div>
   );
 };
