b86b125d604c1d426900932997d832cca985793f
Mahajabin Tokon <tokonislam1122@gmail.com>
2025-01-14 19:20:51 -0700

    stats page complete

--- src/Pages/Dashboard/Statistics.jsx
+++ src/Pages/Dashboard/Statistics.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
-
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import { PieChart, Pie, Legend, Tooltip, ResponsiveContainer } from "recharts";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 const Statistics = () => {
-    return (
-        <div>
-            Stat
-        </div>
-    );
+  const axiosSecure = useAxiosSecure();
+  const { refetch, data: stats = {} } = useQuery({
+    queryKey: ["stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(
+        `${import.meta.env.VITE_API_URL}/stats`
+      );
+      console.log(res.data);
+      return res.data;
+    },
+  });
+
+  const data01 = [
+    { name: "Users", value: stats.users },
+    { name: "Reviews", value: stats.reviews },
+    { name: "Products", value: stats.products },
+  ];
+
+  return (
+    <div className="flex justify-center items-center">
+      <PieChart width={275} height={275}>
+        <Pie
+          dataKey="value"
+          isAnimationActive={false}
+          data={data01}
+          cx="50%"
+          cy="50%"
+          outerRadius={80}
+          fill="#8884d8"
+          label
+        />
+        <Legend></Legend>
+        <Tooltip />
+      </PieChart>
+    </div>
+  );
 };
 
 export default Statistics;
