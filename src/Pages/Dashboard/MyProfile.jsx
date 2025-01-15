80ce24c2acec199b947ef612b3dccf1a94decc8c
Mahajabin Tokon <tokonislam1122@gmail.com>
2025-01-15 12:28:08 -0700

    Adding coupon for discount logic added

--- src/Pages/Dashboard/MyProfile.jsx
+++ src/Pages/Dashboard/MyProfile.jsx
@@ -1,21 +1,57 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { authContext } from "../../AuthProvider/AuthProvider";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useQuery } from "@tanstack/react-query";
+import Swal from "sweetalert2";
 
 const MyProfile = () => {
   const { user } = useContext(authContext);
-  const { refetch, data: theUser = {} } = useQuery({
+  const [amount, setAmount] = useState(50);
+  const { data: theUser = {} } = useQuery({
     queryKey: ["theUser"],
     queryFn: async () => {
       const res = await axios.get(
         `${import.meta.env.VITE_API_URL}/users/single/${user?.email}`
       );
-      console.log(res.data)
+      // console.log(res.data);
       return res.data;
     },
   });
+
+  const { data: coupons = [] } = useQuery({
+    queryKey: ["coupons"],
+    queryFn: async () => {
+      const res = await axios.get(`${import.meta.env.VITE_API_URL}/coupons`);
+      // console.log(res.data);
+      return res.data;
+    },
+  });
+
+  const handleCoupon = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const code = form.code.value;
+
+    const theCoupon = coupons.find(
+      (eachCoupon) => eachCoupon.couponCode === code
+    );
+    if (theCoupon) {
+      const discount = theCoupon?.discount;
+      const newAmount = amount - discount;
+      setAmount(newAmount);
+      console.log(amount);
+    } else {
+      Swal.fire({
+        position: "top-end",
+        icon: "error",
+        title: "The coupon code is wrong",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }
+  };
+
   return (
     <div className="card bg-base-100 shadow-xl">
       <figure>
@@ -29,12 +65,29 @@ const MyProfile = () => {
           <Link
             // to="/dashboard/payment"
             to="/dashboard/payment"
-            state={{ amount: 50 }}
+            state={{ amount }}
             className="btn"
           >
-            $50
+            ${amount}
           </Link>
         </div>
+        <form
+          className="py-10 flex justify-start items-end gap-2 rounded-xl"
+          onSubmit={handleCoupon}
+        >
+          <div className="">
+            <p>Coupon Code</p>
+            <input
+              name="code"
+              type="text"
+              placeholder="Coupon Code"
+              className="input input-bordered"
+            />
+          </div>
+          <div className="">
+            <input type="submit" value="Add" className="btn" />
+          </div>
+        </form>
       </div>
     </div>
   );
